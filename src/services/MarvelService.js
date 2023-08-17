a49eff77d061bfeb5c8274d3ab4c69dc5bc8cb81
kira <k-korni1ov@yandex.ru>
2023-08-17 13:25:46 +0400

    feat: MarvelService

---
+++ src/services/MarvelService.js
@@ -0,0 +1,16 @@
+import { Config } from "../config.properties";
+
+class MarvelService {
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status : ${res.status}`);
+    }
+    return await res.json();
+  };
+  getAllCharacters = () => {
+    return this.getResource(Config.API);
+  };
+}
+export default MarvelService
