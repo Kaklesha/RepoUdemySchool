194dde589b4dfc652734799e04b0940919374a45
Kaklesha <k-korni1ov@yandex.ru>
2023-08-17 18:22:34 +0400

    init: Func

--- src/components/errorMessage/errorMessage.js
+++
@@ -1,8 +0,0 @@
-import img from "./unlike.svg"
-
-const ErrorMessage=()=>{
-    return (
-        <img src={img} style={{width:"250px", height:"250px", margin: "0 auto"}} alt="ddd"/>
-    )
-}
-export default ErrorMessage ;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import './style/style.scss';
+import './index.css';
+import App from './App';
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>,
   document.getElementById('root')
 );
+
 
