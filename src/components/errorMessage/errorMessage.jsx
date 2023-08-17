edfaa1e42a16e3759dace9c55bca0e014a00326a
kira <k-korni1ov@yandex.ru>
2023-08-17 16:01:44 +0400

    done:   150 task

---
+++ src/components/errorMessage/errorMessage.jsx
@@ -0,0 +1,8 @@
+import img from "./unlike.svg"
+
+const ErrorMessage=()=>{
+    return (
+        <img src={img} style={{width:"250px", height:"250px", margin: "0 auto"}} alt="ddd"/>
+    )
+}
+export default ErrorMessage ;
