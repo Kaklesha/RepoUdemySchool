4e7de2f22c3b5e3a522dfc02a043d162b60d07f0
kira <k-korni1ov@yandex.ru>
2023-08-17 13:58:16 +0400

    done: 148 task

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 import { Config } from "../config.properties";
 
 class MarvelService {
+_apiBase ="https://gateway.marvel.com/v1/public/";
+
   getResource = async (url) => {
     let res = await fetch(url);
 
@@ -10,10 +12,10 @@ class MarvelService {
     return await res.json();
   };
   getAllCharacters = () => {
-    return this.getResource(`https://gateway.marvel.com/v1/public/characters?limit=9&offset=210&apikey=${Config.API}`);
+    return this.getResource(`${this._apiBase}/characters?limit=9&offset=210&apikey=${Config.API}`);
   };
   getAllCharacter = (id) => {
-    return this.getResource(`https://gateway.marvel.com/v1/public/characters/${id}?apikey=${Config.API}`);
+    return this.getResource(`${this._apiBase}/characters/${id}?apikey=${Config.API}`);
 };
 }
 export default MarvelService
